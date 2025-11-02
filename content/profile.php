d5209916e62f23c98df97e31a317967f4edbeb23
JustSomeGuy420 <alwynsterling@gmail.com>
2025-11-02 14:08:32 -0500

    Updated sidebar

--- content/profile.php
+++
@@ -1,8 +0,0 @@
-<h2>Profile</h2>
-
-<form id="profileForm">
-    <input type="text" name="name" placeholder="Your Name"><br><br>
-    <button type="submit">Save</button>
-</form>
-
-<div id="updateMsg"></div>
