a4707c9c5c2035a9b09daa6428098310b9e54a26
JustSomeGuy420 <alwynsterling5@gmail.com>
2025-12-24 01:51:05 -0500

    cleaned main

--- content/home.php
+++
@@ -1,2 +0,0 @@
-<h2>Welcome to Dashboard</h2>
-<p>This content is loaded with AJAX!</p>
--- includes/sidebar.php
+++
@@ -1,6 +0,0 @@
-<aside class="sidebar">
-  <a href="#" class="load-page" data-page="home.php">Home</a>
-  <a href="#" class="load-page" data-page="new_contact.php">New Contact</a>
-  <a href="#" class="load-page" data-page="users.php">Users</a>
-  <a href="logout.php">Logout</a>
-</aside>
